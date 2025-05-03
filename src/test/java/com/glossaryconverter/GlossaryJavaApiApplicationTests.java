e4bcfd9dcd339242a1e981ce794ea9dcc5733ea4
a7medMs-ai <ahmed.mostafsaad@gmail.com>
2025-05-03 03:23:02 +0300

    Add files via upload

---
+++ src/test/java/com/glossaryconverter/GlossaryJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glossaryconverter.glossary_java_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlossaryJavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
